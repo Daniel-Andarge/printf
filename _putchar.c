43947e7dbfc5b08a1a6414ae8d8580e411259d2e
ayenewtilaye <ayenewtilaye065@gmail.com>
2022-05-18 19:17:14 +0300

    put cha

--- _putchar.c
+++ _putchar.c
@@ -1,33 +1,13 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include <unistd.h>
 
-#include <stdarg.h>
 /**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print(Function Prototype)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-typedef struct print
+int _putchar(char c)
 {
-  char *t;
-  int (*f)(va_list);
-} print_t;
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
-
-#endif  /* _MAIN_H */
-
+	return (write(1, &c, 1));
+}
