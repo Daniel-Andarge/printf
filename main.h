fe35daedcfc060e20148626d5b6890147a0f699b
ayenewtilaye <ayenewtilaye065@gmail.com>
2022-05-18 18:57:11 +0300

    main fuction

--- main.h
+++ main.h
@@ -4,13 +4,13 @@
 #include <stdarg.h>
 /**
  * struct print - structure for printing various types
- * @t: type to printf
- * @f: function to printf
+ * @t: type to print
+ * @f: function to print(Function Prototype)
  */
 typedef struct print
 {
-	char *t;
-	int (*f)(va_list);
+  char *t;
+  int (*f)(va_list);
 } print_t;
 
 int _putchar(char c);
